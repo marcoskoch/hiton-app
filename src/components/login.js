d61b75484a17ecf900b01ce4dbef315c45ba9879
Felipe Gomes <felipegomess.fg@gmail.com>
2018-01-10 23:38:14 -0200

    add tela contato

--- src/components/login.js
+++ src/components/login.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import { Text, TouchableOpacity, View, StyleSheet, Dimensions, Image, TextInput, AsyncStorage } from 'react-native';
 import { LoginButton, AccessToken } from 'react-native-fbsdk';
 import axios from 'axios';
+import Modal from 'react-native-modal'
 
 const win = Dimensions.get('window');
 const primaryColor = '#2d7bdc';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalVisible: false
+    };
+  }
+  _toggleModal = () => this.setState({ isModalVisible: !this.state.isModalVisible });
   static navigationOptions = {
     header: null
   };
@@ -24,6 +32,7 @@ class Login extends Component {
         <View style={styles.positionLogo}>
           <Image style={styles.imageLogo}
             source={require('../assets/images/HIT-On.png')} />
+            <Text style={styles.textEntrar}>Hit On</Text>
         </View>
         <View style={styles.positionText}>
           <LoginButton
@@ -57,6 +66,19 @@ class Login extends Component {
             }
             onLogoutFinished={() => alert("User logged out")} />
         </View>
+        <TouchableOpacity style={styles.positionContato} onPress={this._toggleModal}>
+          <Text style={styles.textContato}>Contato</Text>
+        </TouchableOpacity>
+        <Modal isVisible={this.state.isModalVisible}>
+          <View style={styles.container}>
+            <Text style={styles.txtTitulo}>Contato dos desenvolvedores!</Text>
+            <Text style={styles.txtTitulo}>apphiton@gmail.com</Text>
+            <TouchableOpacity onPress={this._toggleModal}>
+              <Text style={styles.txtReturn}>Voltar</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+
 
       </View>
     );
@@ -69,18 +91,62 @@ const styles = StyleSheet.create({
   backgroundLogin: {
     backgroundColor: primaryColor,
     flex: 1,
-    flexDirection: 'column'
-  },
-  positionLogo: {
-    marginTop: 150,
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center'
   },
-  positionText: {
+
+  positionLogo: {
+    marginTop: 75,
     alignItems: 'center',
-    marginTop: 35
+    flex: 4
   },
   imageLogo: {
     width: win.width / 2.5,
     height: win.width / 2.5
-  }
+  },
+  textEntrar: {
+      fontSize: 35,
+      marginTop: 15,
+      color: '#FFFFFF',
+      justifyContent: 'center',
+      alignItems: 'center'
+      // fontFamily: 'segoeuil'
+  },
+  positionContato: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  },
+  textContato: {
+      fontSize: 15,
+      color: '#FFFFFF',
+      // fontFamily: 'segoeuil'
+  },
+  positionText: {
+    alignItems: 'center',
+    marginTop: 35,
+    flex: 5
+  },
+  txtTitulo: {
+    fontSize: 20,
+    color: '#FFF',
+    marginBottom: 20,
+    //fontFamily: 'segoeuil'
+  },
+  txtReturn: {
+    fontSize: 15,
+    marginTop: 15,
+    color: '#FFF'
+    //fontFamily: 'segoeuil'
+  },
+  container: {
+    flex: 1,
+    marginTop: 50,
+    marginBottom: 50,
+    marginLeft: 10,
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
 });
